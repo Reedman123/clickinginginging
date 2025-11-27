e3bd6702f7ba17e9d90d8e52b47387d78afa0e91
Reedman123 <114189712+Reedman123@users.noreply.github.com>
2025-11-27 07:16:26 +0000

    nur

--- scripts.js
+++ scripts.js
@@ -1,11 +1,25 @@
 let clicks = 0;
-
+let upgrades = 0;
+let price = 10;
 document.getElementById('clicker').onclick = function () 
 {
   clicks++
   document.getElementById("rizz").textContent = clicks
 }
-
+document.getElementById('upgrade').onclick = function ()
+{
+  if (clicks >= price) {
+    clicks=clicks-price
+    price=price+10
+    upgrades++
+    document.getElementById("cps").textContent = upgrades
+    document.getElementById("rizz").textContent = clicks
+  }
+}
+setInterval(function(){
+  clicks=clicks+upgrades
+  document.getElementById("rizz").textContent = clicks
+},1000)
 
 document.addEventListener("keypress", function(event) {
     if (event.key == "c") {
@@ -13,4 +27,4 @@ document.addEventListener("keypress", function(event) {
 song.play();
     }
 });
-document.getElementById("rizz").textContent = clicks
+
